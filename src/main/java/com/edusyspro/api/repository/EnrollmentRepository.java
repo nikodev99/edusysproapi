b959f7dcde56dcd0cd895cc8bfe6540fc9dc5ea8
nikodev99 <nikhe.niama99@outlook.fr>
2024-08-19 16:35:13 +0100

    Ref: Tiny amend from how a student's classmates are retrieved

--- src/main/java/com/edusyspro/api/repository/EnrollmentRepository.java
+++ src/main/java/com/edusyspro/api/repository/EnrollmentRepository.java
@@ -50,9 +50,9 @@ public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Lo
             select new com.edusyspro.api.dto.EnrolledStudent(e.student.id, e.academicYear, e.student.firstName, e.student.lastName, \
             e.student.gender, e.student.emailId, e.student.birthDate, e.student.birthCity, e.student.nationality, e.student.reference, \
             e.student.image, e.enrollmentDate, e.classe.name, e.classe.grade.section, e.student.dadName, e.student.momName) from EnrollmentEntity e \
-            where e.academicYear.school.id = ?1 and e.student.id <> ?2 and e.classe.id = (select en.classe.id from EnrollmentEntity en where en.student.id = ?2) and e.academicYear.current = true and e.isArchived = false order by rand()
+            where e.academicYear.school.id = ?1 and e.student.id <> ?2 and e.classe.id = ?3 and e.academicYear.current = true and e.isArchived = false order by function('rand')
    """)
-    Page<EnrolledStudent> findStudentRandomClassmateByClasseId(UUID schoolId, UUID studentId, Pageable pageable);
+    Page<EnrolledStudent> findStudentRandomClassmateByClasseId(UUID schoolId, UUID studentId, int classeId, Pageable pageable);
 
     @Query("select new com.edusyspro.api.dto.EnrolledStudentGuardian(e.student.guardian.id, e.student.guardian.firstName, " +
             "e.student.guardian.lastName, e.student.guardian.maidenName, e.student.guardian.genre, e.student.guardian.emailId, " +
